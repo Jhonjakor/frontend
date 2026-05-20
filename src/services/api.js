424d6f5cfab71efdb22a2482207220a6b880a1a0
p0zet1ve <dmitrii.petrov122@gmail.com>
2026-05-20 19:38:28 +0300

    ggg

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || 'https://cinemaapi-bd3i.onrender.com';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://cinemaapi-fn19.onrender.com';
 
 // ── Утилита запросов ─────────────────────────────────────────
 async function request(path, options = {}) {
